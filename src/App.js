54d4f931693757c51cd1d3e82bf0135a23a7feb8
Lisa Wagner <wagnerl@mskcc.org>
2019-07-30 11:09:04 +0200

    setup

--- src/App.js
+++ src/App.js
@@ -1,37 +1,34 @@
 import React from "react";
-import logo from "./logo.svg";
-import Button from "@material-ui/core/Button";
+import ReactDOM from "react-dom";
+import "react-app-polyfill/ie9";
+import "react-app-polyfill/ie11";
 
-import "./App.css";
+import { LocalizeProvider } from "react-localize-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { store, persistor } from "./store/configureStore";
+
+import Root from "./containers/Root";
+
+import * as serviceWorker from "./serviceWorker";
+
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button
-          onClick={() => {
-            alert("alert");
-          }}
-          variant="contained"
-          color="secondary"
-        >
-          Anna's Button
-        </Button>
-      </header>
-    </div>
+    <Provider store={store}>
+      <LocalizeProvider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Root />
+        </PersistGate>
+      </LocalizeProvider>
+    </Provider>
   );
 }
 
 export default App;
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
