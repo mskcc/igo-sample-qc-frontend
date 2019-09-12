131a9c7f954d2fe5a6be12bef6fe942d00539d56
Lisa Wagner <wagnerl@mskcc.org>
2019-09-12 13:21:53 -0400

    Sidebar and SearchField

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,67 +1,71 @@
-import React from 'react';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import InputBase from "@material-ui/core/InputBase";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
 
-import TextField from '@material-ui/core/TextField';
-
+import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles(theme => ({
   container: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    textAlign: "center",
+    gridArea: "sidebar",
+    height: "92vh",
+    backgroundColor: "#eceff1",
+    display: "grid",
+    gridTemplate: "'search' 'tree'",
+    justifyItems: "center"
   },
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
+  search: {
+    padding: "2px 4px",
+    marginTop: "1em",
+    gridArea: "search",
+    display: "flex",
+    height: "min-content",
+    alignItems: "center",
+    maxWidth: "80%"
   },
-  dense: {
-    marginTop: theme.spacing(2),
-  },
-  menu: {
-    width: 200,
-  },
+  iconButton: {
+    padding: 10
+  }
 }));
 
-export default function OutlinedTextFields() {
+export default function Sidebar(props) {
   const classes = useStyles();
-  const [values, setValues] = React.useState({
-    name: 'Cat in the Hat',
-    age: '',
-    multiline: 'Controlled',
-    currency: 'EUR',
-  });
 
-  const handleChange = name => event => {
-    setValues({ ...values, [name]: event.target.value });
+  const [values, setValues] = React.useState({ requestId: "" });
+
+  const handleChange = requestId => event => {
+    setValues({ ...values, [requestId]: event.target.value });
+  };
+
+  const handleSubmit = () => {
+    props.handleSubmit(values.requestId);
   };
 
   return (
-    <form className={classes.container} noValidate autoComplete="off">
-     
-      <TextField
-        id="outlined-multiline-flexible"
-        label="Multiline"
-        multiline
-        rowsMax="4"
-        value={values.multiline}
-        onChange={handleChange('multiline')}
-        className={classes.textField}
-        margin="normal"
-        helperText="hello"
-        variant="outlined"
-      />
-      <TextField
-        id="outlined-multiline-static"
-        label="Multiline"
-        multiline
-        rows="4"
-        defaultValue="Default Value"
-        className={classes.textField}
-        margin="normal"
-        variant="outlined"
-      />
-     
-      
-    </form>
+    <div className={classes.container}>
+      <Paper className={classes.search}>
+        <InputBase
+          className={classes.textField}
+          id="outlined-requestId"
+          value={values.requestId}
+          placeholder="Request ID"
+          onChange={handleChange("requestId")}
+          inputProps={{ "aria-label": "search google maps" }}
+        />
+        <IconButton
+          className={classes.iconButton}
+          onClick={handleSubmit}
+          aria-label="search"
+        >
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+    </div>
   );
 }
---
+++ src/containers/SidebarContainer.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import { withLocalize } from "react-localize-redux";
+import { connect } from "react-redux";
+import { reportActions } from "../actions";
+import { Redirect } from "react-router-dom";
+
+import { Sidebar } from "../components";
+
+export class SidebarContainer extends Component {
+  handleSubmit = requestId => {
+    console.log(requestId)
+    this.props.getRequest(requestId);
+  };
+  // handleGridSubmit = formValues => {
+  //   this.props.addGridToBankedSample(this.props);
+  // };
+
+
+  render() {
+    return (
+      <React.Fragment>
+        <Sidebar handleSubmit={this.handleSubmit}/>
+      </React.Fragment>
+    );
+  }
+}
+
+SidebarContainer.defaultProps = {};
+
+const mapStateToProps = state => ({});
+
+export default withLocalize(
+  connect(
+    mapStateToProps,
+    {
+      ...reportActions,
+    }
+  )(SidebarContainer)
+);
