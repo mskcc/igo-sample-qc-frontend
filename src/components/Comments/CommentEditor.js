fc71367b132e112824ab58411ef5dea4138c7c09
Stephanie DelBelso <sdelbelso@grubhub.com>
2023-04-04 14:13:11 -0400

    empty

--- src/components/Comments/CommentEditor.js
+++ src/components/Comments/CommentEditor.js
@@ -73,7 +73,7 @@ export default function CommentEditor(props) {
     'RNA Report': false,
     'Library Report': false,
     salutation: '',
-    addressee: props.request.investigatorName.split(' ')[0],
+    addressee: props.request.investigatorName.split(' ')[0] || '',
     downstreamProcess:
       props.currentReportShown === 'Library Report' ||
       props.currentReportShown === 'Pool Report'
