b5896b644f95913c613a70d321a2196652556a57
Stephanie DelBelso <sdelbelso@grubhub.com>
2023-04-19 14:24:11 -0400

    open new request window

--- src/containers/Report/PendingContainer.js
+++ src/containers/Report/PendingContainer.js
@@ -13,8 +13,9 @@ export class PendingContainer extends Component {
     }
   }
   showPending = (request) => {
-    this.props.history.push('/request/' + request);
-    this.props.getRequest(request);
+    window.open(`/request/${request}`, '_blank');
+    // this.props.history.push('/request/' + request);
+    // this.props.getRequest(request);
   };
   render() {
     return (
