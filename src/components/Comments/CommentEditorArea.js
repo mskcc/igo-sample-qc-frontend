8e7a7eacf1de24c70092870624805a553237628e
Stephanie DelBelso <sdelbelso@grubhub.com>
2023-08-24 10:47:55 -0400

    remove prop

--- src/components/Comments/CommentEditorArea.js
+++ src/components/Comments/CommentEditorArea.js
@@ -68,7 +68,6 @@ export default function CommentEditorArea(props) {
           recipe={props.recipe}
           currentReportShown={props.currentReportShown}
           request={props.request}
-          recipients={props.recipients}
           tables={props.tables}
           comments={props.comments}
           recipientsBeingEdited={values.recipientsBeingEdited}
